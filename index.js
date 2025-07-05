6d4d49ed9d115a0371aa5b3537e536ed5442b811
Akash <hafejmohammadakash@gmail.com>
2025-07-05 12:38:59 +0600

    token pelam cookie te

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // midle ware
-app.use(cors());
+app.use(cors({
+  origin: `http://localhost:5173`,
+  credentials: true,
+}));
 app.use(express.json());
 
 // mongoDB server site Code:
